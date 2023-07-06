2d52bb4fa91994e28682c89cca5b2dfd9dfde232
amr <alikhaledomar8@gmail.com>
2023-07-06 14:55:17 +0300

    sec commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
-import './App.css';
 import Layout from './Components/Layout/Layout';
-import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import {RouterProvider, createHashRouter} from 'react-router-dom'
 import Home from './Components/home/home';
 import Regester from './Components/Regester/Regester';
 import Login from './Components/Login/Login';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import jwtDecode from 'jwt-decode'
 import Moviedetails from './Components/Moviedetails/Moviedetails';
 import Movies from './Components/Movies/Movies';
@@ -29,19 +27,15 @@ function saveUserData(){
 }
 
 
-let routers =createBrowserRouter([
+let routers =createHashRouter([
   {path:'' ,element:<Layout setuserData={setuserData} userData={userData}/> ,children:[
     {index:true , element:<Home/>},
-    
     {path:'movies' , element:<Movies/>},
     {path:'tvshowe' , element:<Tvshowe/>},
     {path:'people' , element:<People/>},
-
     {path:'moviedetails/:id/:mediaType' , element:<Moviedetails />},
     {path:'regester' , element:<Regester/>},
-
     {path:'login' , element:<Login saveUserData={saveUserData}/>},
-
   ]}
 ])
 
